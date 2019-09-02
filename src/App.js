6ec25f9af65c9133ecb5c487b1ef4c6d900613e8
Chingiz Mamedov <38461842+chingiz92mi@users.noreply.github.com>
2019-09-02 10:47:34 +0500

    set music any

--- src/App.js
+++ src/App.js
@@ -4,25 +4,32 @@ import { connect } from "react-redux";
 import { BrowserRouter as HashRouter } from "react-router-dom";
 import Layout from './components/Layout/Layout';
 import { setTime } from './actions';
-import Music from './images/aaa.mp3'
+import Music from './images/aaa.mp3';
 
 
 class App extends Component {
     constructor(props) {
         super(props)
-
+        this.state = {
+            music: true
+        }
         this.audio = new Audio(Music);
 
     }
 
-
+    playMusic = () => {
+        this.setState({
+            music: !this.state.music
+        })
+        this.state.music ? this.audio.pause() : this.audio.play()
+    }
     
+
     componentWillMount() {
-        setTimeout(()=> {
+        window.onload = () => {
+            this.audio.loop = true;
             this.audio.play()
-            console.log('play')
-        }, 1000)
-        
+        }
         
 
         fetch('http://innova-team.ru/bend/wp-json/wp/v2/posts/')
@@ -38,7 +45,15 @@ class App extends Component {
         
         return (
             <HashRouter>
-              <Layout />    
+              <Layout />
+              <div className="music-icon" onClick={this.playMusic.bind(this)}>
+                {
+                    this.state.music ? <img src="assets/icon/av/volume_up_24px_outlined.png" alt=""/> :
+                    <img src="assets/icon/av/volume_off_24px_outlined.png" alt=""/>
+                }
+                  
+                  
+              </div>
             </HashRouter>
         )
     }
